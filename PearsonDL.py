0c87aa6359da5284691e33d451f7393e989fb4a6
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2020-04-01 23:54:32 +1100

    Add files via upload

---
+++ PearsonDL.py
@@ -0,0 +1,25 @@
+import os
+import urllib.request
+import os.path
+from os import path
+
+print("PearsonDL")
+print("Developed by NamesJoeyWheeler & FurryLovingMcNugget")
+print("---------------------------------------------------")
+id = input("Book ID: ")
+pages = int(input("How many pages would you like? "))
+
+if os.path.isfile('Pearson Books/') and os.access('Pearson Books/', os.R_OK):
+    for i in range(0, pages):
+        pb = f'https://d38l3k3yaet8r2.cloudfront.net/resources/products/epubs/generated/{id}/foxit-assets/pages/page{i}?password=&accessToken=null&formMode=true'
+        urllib.request.urlretrieve(pb, f'Pearson Books/{id}/{i}.jpg')
+        print('Downloading...')
+else:
+    directory = id
+    parent_dir = 'Pearson Books/'
+    path = os.path.join(parent_dir, directory)
+    os.makedirs(path)
+    for i in range(0, pages):
+        pb = f'https://d38l3k3yaet8r2.cloudfront.net/resources/products/epubs/generated/{id}/foxit-assets/pages/page{i}?password=&accessToken=null&formMode=true'
+        urllib.request.urlretrieve(pb, f'Pearson Books/{id}/{i}.jpg')
+        print('Downloading...')
